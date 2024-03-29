2457bb1c97a3f9710db68c2bfef8a093c1d88af8
MarckDmarck <155605126+MarckDmarck@users.noreply.github.com>
2024-03-29 20:15:56 +0000

    se realizo el proyecto RandomCard

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbersCard = ["As", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Rey", "Reina", "Jota"];
+
+  let stickCard = [
+    { Icon: "♦", color: "red" },
+    { Icon: "♥", color: "red" },
+    { Icon: "♣", color: "black" },
+    { Icon: "♠", color: "black" }
+  ];
+
+  document.querySelector(".value").innerHTML =
+    numbersCard[Math.floor(Math.random() * numbersCard.length)];
+
+  let stickCard2 = stickCard[Math.floor(Math.random() * stickCard.length)];
+  document.querySelectorAll(".palo").forEach(element => {
+    element.innerHTML = stickCard2.Icon;
+    element.style.color = stickCard2.color;
+  });
 };
